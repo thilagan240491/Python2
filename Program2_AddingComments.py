a9d3ca491ea5e4f57a8389c0dee8b7c10dd70206
thilagan240491 <thilagan24@gmail.com>
2026-02-15 22:15:45 +0530

    This is program2 from alpha branch

---
+++ Program2_AddingComments.py
@@ -0,0 +1,16 @@
+
+
+#Colon is used to add a single line comment
+
+''' Triple
+coats
+is used
+to
+add
+a multi-line comment '''
+
+""" Trying to add
+multi-line comment with
+double-coats """
+
+print('this is how you add a comment in a python code')
