ed9e9baa36b0d72f25dd5b2cdecc4c7645f5f7ad
thilagan240491 <thilagan24@gmail.com>
2026-02-15 22:24:52 +0530

    this is from branch beta

---
+++ Program3_CelciusToFahrenheit.py
@@ -0,0 +1,9 @@
+#this is a program to convert Celcius to Fahrenheit
+
+
+x=int(input("Please enter the value of temperature in Celcius:"))
+
+
+y=((9/5)*x)+32
+
+print("The temperature in Fahrenheit is:",y)
